1abb5dd67d278abd6de361da969395e0ff2c0a94
Ari Nurcahya <nurcahyaari@gmail.com>
2019-07-20 08:54:10 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
+import "bootstrap";
 import "./styles.css";
 
 class App extends Component {
@@ -12,6 +12,14 @@ class App extends Component {
       }
     ]
   };
+  delForm(e) {
+    this.state.form.pop();
+    let form = this.state.form;
+    console.log(form);
+    this.setState({
+      form: form
+    });
+  }
   addForm(e) {
     this.state.form.push({
       name: "",
@@ -25,58 +33,82 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <form>
-          {this.state.form.map((val, i) => {
-            return (
-              <div className="row" key={i}>
-                <div className="col-md">
-                  <label>Name</label>
-                  <input
-                    type="text"
-                    value={val.name}
-                    onChange={e => {
-                      let form = this.state.form;
-                      form[i].name = e.target.value;
-                      this.setState({
-                        form: form
-                      });
-                    }}
-                  />
+      <div className="container">
+        <div className="row">
+          <form clasName="form-control">
+            {this.state.form.map((val, i) => {
+              let deleteForm;
+              if (i > 0) {
+                deleteForm = (
+                  <div className="row">
+                    <div className="col-md-4">
+                      <input
+                        type="button"
+                        onClick={e => {
+                          this.delForm(e);
+                        }}
+                        value="Hapus form"
+                      />
+                    </div>
+                  </div>
+                );
+              }
+              return (
+                <div className="col-md col-sm col-xs" key={i}>
+                  <div className="form-group">
+                    <label for="text">Name</label>
+                    <input
+                      type="text"
+                      clasName="input"
+                      value={val.name}
+                      onChange={e => {
+                        let form = this.state.form;
+                        form[i].name = e.target.value;
+                        this.setState({
+                          form: form
+                        });
+                      }}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Age</label>
+                    <input
+                      type="number"
+                      value={val.age}
+                      onChange={e => {
+                        let form = this.state.form;
+                        form[i].age = e.target.value;
+                        this.setState({
+                          form: form
+                        });
+                      }}
+                    />
+                  </div>
+                  {deleteForm}
                 </div>
-                <div className="col-md">
-                  <label>Age</label>
-                  <input
-                    type="number"
-                    value={val.age}
-                    onChange={e => {
-                      let form = this.state.form;
-                      form[i].age = e.target.value;
-                      this.setState({
-                        form: form
-                      });
-                    }}
-                  />
-                </div>
-              </div>
-            );
-          })}
-          <input
-            type="submit"
-            onClick={e => {
-              e.preventDefault();
-              console.log(this.state.form);
-            }}
-            value="kirim"
-          />
-        </form>
-        <input
-          type="button"
-          onClick={e => {
-            this.addForm(e);
-          }}
-          value="Tambah form"
-        />
+              );
+            })}
+            <input
+              type="submit"
+              onClick={e => {
+                e.preventDefault();
+                console.log(this.state.form);
+              }}
+              value="kirim"
+            />
+          </form>
+          <div className="row">
+            <div className="col-md-4">
+              <input
+                type="button"
+                onClick={e => {
+                  this.addForm(e);
+                }}
+                value="Tambah form"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
